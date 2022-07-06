c00123a5d57e6125ddbd5560255487170f0e0843
majic <871133493@qq.com>
2022-07-06 16:05:20 +0800

    第一次提交

---
+++ src/main/java/com/majic/gittest/TestGit.java
@@ -0,0 +1,11 @@
+package com.majic.gittest;
+
+/**
+ * @author maojiuchao
+ * @date 2022/7/6 16:03
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+    }
+}
